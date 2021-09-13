7dc924076ae1c2c47920effd8fb32c6bbb4c0414
Meng-Yuan Huang <myh@live.com>
2021-09-13 09:55:45 +0800

    * Fix a possible bug.

--- src/pages/SettingsPage.tsx
+++ src/pages/SettingsPage.tsx
@@ -276,7 +276,7 @@ class _SettingsPage extends React.Component<PageProps, StateProps> {
                     this.props.dispatch({
                       type: "SET_KEY_VAL",
                       key: 'uiFontSize',
-                      val: e.detail.value,
+                      val: +e.detail.value,
                     });
                     Globals.updateCssVars(this.props.settings);
                   }} />
