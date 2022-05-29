89cdeee0007ee3ec739ecd79a8d27fb99acc6592
Meng-Yuan Huang <myh@live.com>
2022-05-29 09:20:41 +0800

    * Rewrite codes.

--- src/redux/reducers/index.tsx
+++ src/redux/reducers/index.tsx
@@ -1,8 +1,8 @@
-import { combineReducers } from "redux"
+import { combineReducers } from '@reduxjs/toolkit';
 
-import settings from "./setReducer"
-import tmpSettings from "./tmpSetReducer"
+import settings from './setReducer';
+import tmpSettings from './tmpSetReducer';
 
 export default combineReducers({
   settings, tmpSettings
-})
+});
